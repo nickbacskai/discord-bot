d7b896718dd38975bdddbb812b8f6b61f5db6f5e
Nick <nbacskai@gmail.com>
2020-06-05 14:44:18 -0400

    no .env

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 require("dotenv").config()
 const Discord = require("discord.js")
+const fs = require("fs")
 const client = new Discord.Client()
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
-  }
+fs.readdir("./events/", (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`)
+    const eventName = file.split(".")[0]
+    client.on(eventName, (...args) => eventHandler(client, ...args))
+  })
 })
 
 client.login(process.env.BOT_TOKEN)
